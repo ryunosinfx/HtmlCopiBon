462a48e39e98c659d6fe4385e4280e088ee9d01f
ryunosinfx <ryunosinfx@livedoor.com>
2018-06-11 10:04:21 +0900

    itatokoromade

--- src/util/reactive/base/vtags.js
+++ src/util/reactive/base/vtags.js
@@ -1,4 +1,7 @@
-import {patch, h} from './preLoader'
+import {
+  patch,
+  h
+} from './preLoader'
 export class V {
   static div() {}
 }
@@ -29,58 +32,61 @@ const setProps(data, inputs) {
 }
 const vtags = (tagName, id, classNames, text, children, data = {}) => {
   const id1 = tagName + (
-    "#" + id
-    ? id
-    : "");
+    "#" + id ?
+    id :
+    "");
   const id2 = id1 + (
-    classNames
-    ? (
-      Array.isArray(classNames)
-      ? classNames.join(".")
-      : classNames)
-    : "");
-  const childrenArray = children && Array.isArray(children)
-    ? children
-    : [];
+    classNames ?
+    (
+      Array.isArray(classNames) ?
+      classNames.join(".") :
+      classNames) :
+    "");
+  const childrenArray = children && Array.isArray(children) ?
+    children : [];
 
   return h(id2, data, childrenArray, text);
 }
-const vtagsA = ( id, classNames, text, children,data) => {
+// string array string array object
+// string objct array string
+const vtagsA = (id, classNames, children, text, data) => {
   let count = 5;
-  count += data===undefined?0:-1;
-  count += children===undefined?0:-1;
-  count += text===undefined?0:-1;
-  count += classNames===undefined?0:-1;
-  count += id===undefined?0:-1;
-  if(count === 4){
-    const currentData = data?data:{}
-    const currentId = Array.isArray(id)? null:id;
-    const currentClasss = Array.isArray(id)?id:classNames;
-    const currentText = typeof text === "string"?text:"":
-    const currentChildren = Array.isArray(text)?text:children:
+  count += data === undefined ? 0 : -1;
+  count += children === undefined ? 0 : -1;
+  count += classNames === undefined ? 0 : -1;
+  count += text === undefined ? 0 : -1;
+  count += id === undefined ? 0 : -1;
+  if (count === 4) {
+    const currentData = data ? data : {}
+    const currentId = Array.isArray(id) ? null : id;
+    const currentClasss = Array.isArray(id) ? id : classNames;
+    const currentText = typeof text === "string" ? text : "";
+      const currentChildren = Array.isArray(text) ? text : children:
   }
-  if(count === 3){//id children data
-    const currentData = data?data:{}
-    const currentId = Array.isArray(id)? null:id;
-    const currentClasss = Array.isArray(id)?id:(Array.isArray(classNames)?classNames:[]);
-    const currentText = typeof text === "string"?text:"":
-    const currentChildren = Array.isArray(text)?text:children:
+  if (count === 3) { //id children data
+    const currentData = data ? data : {}
+    const currentId = Array.isArray(id) ? null : id;
+    const currentClasss = Array.isArray(id) ? id : (Array.isArray(classNames) ? classNames : []);
+    const currentText = typeof text === "string" ? text : "";
+    const currentChildren = Array.isArray(text) ? text : children:
   }
-  if(Array.isArray(id)){
+  if (Array.isArray(id)) {
 
   }
   return h(id2, data, childrenArray, text);
 }
 //a div span img ul li input label
 export const a = (id, classNames, href, children, data) => {
-  return vtags("a", id, classNames, null, children, setProps(data, {href: href}));
+  return vtags("a", id, classNames, null, children, setProps(data, {
+    href: href
+  }));
 }
-export const div = (id, classNames, text, children, data) => {
-  return vtags("div", id, classNames, text, children, data);
+export const div = (id, classNames, children, text, data) => {
+  return vtags("div", id, classNames, children, text, data);
 }
 
-export const span = (id, classNames, text, children, data) => {
-  return vtags("span", id, classNames, text, children, data);
+export const span = (id, classNames, children, text, data) => {
+  return vtags("span", id, classNames, children, text, data);
 }
 
 export const img = (id, classNames, alt, src, data) => {
@@ -90,17 +96,17 @@ export const img = (id, classNames, alt, src, data) => {
   }));
 }
 
-export const ul = (id, classNames, text, children, data) => {
-  return vtags("ul", id, classNames, text, children, data);
+export const ul = (id, classNames, children, text, data) => {
+  return vtags("ul", id, classNames, children, text, data);
 }
 
-export const li = (id, classNames, text, children, data) => {
-  return vtags("li", id, classNames, text, children, data);
+export const li = (id, classNames, children, text, data) => {
+  return vtags("li", id, classNames, children, text, data);
 }
 
-export const input = (id, classNames, type, value, data) => {
-  return vtags("input", id, classNames, text, children, data);
+export const input = (id, classNames, children, text, data) => {
+  return vtags("input", id, classNames, children, text, data);
 }
-export const label = (id, classNames, text, children, data) => {
-  return vtags("label", id, classNames, text, children, data);
+export const label = (id, classNames, children, text, data) => {
+  return vtags("label", id, classNames, children, text, data);
 }
