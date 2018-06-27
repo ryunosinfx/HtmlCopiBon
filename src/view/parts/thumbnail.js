94615e150ad8f668453632035b80061db3d93af0
ryunosinfx <ryunosinfx@livedoor.com>
2018-06-27 21:17:00 +0900

    dekitatokoromade

--- src/view/parts/thumbnail.js
+++ src/view/parts/thumbnail.js
@@ -3,14 +3,15 @@ import {BaseView} from "../../util/reactive/baseView";
 import {a,div,li,ul,img,span,input,label} from "../../util/reactive/base/vtags";
 import {ImageActionCreator} from '../../reduxy/action/imageActionCreator'
 export class Thumnail  extends BaseView {
-  constructor(index) {
-    super("Thumnail"+index, "Thumnail");
-    this.index= index ;
+  constructor(pk) {
+    super("Thumnail"+pk, "Thumnail");
+    this.pk = pk ;
+  }
+  setImageData(imageData){
+    this.imageData = imageData;
   }
   render(store, actionData) {
-    this.dataLine = div(this.id+"_dataLine_"+this.index);
-    this.delButton = span(this.id+"_delButton_"+this.index, "delButton", "☓");
-    return div(this.id, "Thumnails");
+    return div('' ,"Thumnails");
   }
   remove(pk) {
     return (event)=>{
@@ -19,5 +20,27 @@ export class Thumnail  extends BaseView {
         this.dispatch(action);
       }
     }
+  }
+
+  async crateDataLine(imageData,currentVnode) {
+    const imageEntity = imageData.imageEntity;
+    const binaryEntity = imageData.binaryEntity;
+    console.log(binaryEntity)
+    const data = {
+      name: imageEntity.name,
+      ab: binaryEntity._ab,
+      type: imageEntity.type
+    }
+    const imgElm = await this.ip.createImageNodeByData(data).catch((e) => {
+      console.log(e);
+      throw e
+    });
+    const pk = imageEntity.getPk();
+    const imgVnode = img(pk, imageEntity.name, imageEntity.name, imgElm.src, {});
+    const textVnode = span(pk + "_", ["images"], imageData.imageText);
+    const delButton = span(pk + "_delButton", ["delButton"], {on:{"click":this.remove(pk)}},"x");
+    const dataLineVnode = div("", ["dataLine"], [delButton, imgVnode, textVnode]);
+    const rowVnode = div("", ["row"],[dataLineVnode]);
+    return rowVnode;
   }
 }
--- src/view/parts/thumbnails.js
+++ src/view/parts/thumbnails.js
@@ -63,26 +63,7 @@ export class Thumbnails extends BaseView {
       }
     }
   }
-
-  async crateDataLine(imageData) {
-    const imageEntity = imageData.imageEntity;
-    const binaryEntity = imageData.binaryEntity;
-    console.log(binaryEntity)
-    const data = {
-      name: imageEntity.name,
-      ab: binaryEntity._ab,
-      type: imageEntity.type
-    }
-    const imgElm = await this.ip.createImageNodeByData(data).catch((e) => {
-      console.log(e);
-      throw e
-    });
-    const pk = imageEntity.getPk();
-    const imgVnode = img(pk, imageEntity.name, imageEntity.name, imgElm.src, {});
-    const textVnode = span(pk + "_", ["images"], imageData.imageText);
-    const delButton = span(pk + "_delButton", ["delButton"], {on:{"click":this.remove(pk)}},"x");
-    const dataLineVnode = div("", ["dataLine"], [delButton, imgVnode, textVnode]);
-    const rowVnode = div("", ["row"],[dataLineVnode]);
-    return rowVnode;
+  async crateDataLine(){
+    
   }
 }
