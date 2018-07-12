b09cb70e26995bb0413afb53c2bd74a628793432
ryunosinfx <ryunosinfx@livedoor.com>
2018-07-12 19:48:06 +0900

    dekitatokoromade

--- src/view/content/titleSettings.js
+++ src/view/content/titleSettings.js
@@ -42,11 +42,31 @@ export class TitleSettings extends BaseView {
     console.log(setting);
     console.log(outputProfiles);
     const title = div("",[this.id +"Title"],"Setting for Output");
-    const frame = div("",[this.id +"Frame"]);
-    const button = div("",[this.id +"Update"],{on:{click:this.update()}},"Save!");
+
+    // this.name = "";
+    // this.pageNum = 0;
+    // this.startPage = 0;
+    // this.outputProfile = null;
+    // this.createDate = Date.now();
+    // this.updateDate = Date.now();
+    const nameLabel = span("",[this.id +"Label"],'name:');
+    const nameInput = input("",[this.id +"Label"],{porp:{value:setting.name}});
+    const nameRow = div("",[this.id +"Row"],[nameLabel,nameInput]);
+    const pageNumLabel = span("",[this.id +"Label"],'pageNum:');
+    const pageNumInput = input("",[this.id +"Label"],{porp:{value:setting.pageNum}});
+    const pageNumRow = div("",[this.id +"Row"],[pageNumLabel,pageNumInput]);
+    const startPageLabel = span("",[this.id +"Label"],'startPage:');
+    const startPageInput = input("",[this.id +"Label"],{porp:{value:setting.startPage}});
+    const startPageRow = div("",[this.id +"Row"],[startPageLabel,startPageInput]);
+    const outputProfileLabel = span("",[this.id +"Label"],'outputProfile:');
+    const outputProfileInput = input("",[this.id +"Label"],{porp:{value:setting.outputProfile}});
+    const outputProfileRow = div("",[this.id +"Row"],[outputProfileLabel,outputProfileInput]);
+    const frameL = div("",[this.id +"FrameL"],[nameRow,pageNumRow]);
+    const frameR = div("",[this.id +"FrameR"],[startPageRow,outputProfileRow]);
+    const frame = div("",[this.id +"Frame"],[frameL,frameR]);
+    const button = div("",[this.id +"Update"],{on:{click:this.update()}},"Save! LastUpdae:"+this.updateDate);
     const childlen = [title,frame,button];
     this.prePatch("#"+this.bodyId, div(this.bodyId,["TitleSettings"], childlen));
-      console.log("TitleSettings showSettings");
   }
 
 }
