d12c8b7734d912c48438c05c2b72efd761c68aa7
ryunosinfx <ryunosinfx@livedoor.com>
2018-09-12 22:53:31 +0900

    バージョンうぷ

--- src/view/header.js
+++ src/view/header.js
@@ -16,7 +16,7 @@ export class Header extends BaseView {
   constructor(titleText) {
     super("header", "header");
     this.titleText = titleText;
-    this.Version = "v0.01alpha";
+    this.Version = "v0.02alpha";
   }
   render() {
     const title = span("",["MainTitle"],this.titleText);
