6b4b4428eb43662b445e2e47076ea4751decf3e1
ryunosinfx <ryunosinfx@livedoor.com>
2018-09-18 22:48:43 +0900

    バージョンアップ

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 import css from "../index.css";
-import {MainFrame} from "./view/mainFrame"
-import {MainService} from "./service/mainService"
+import { MainFrame } from "./view/mainFrame"
+import { MainService } from "./service/mainService"
 const appTitle = "CopiBon";
-const appVersion = "v0.0.4alpha";
+const appVersion = "v0.0.5alpha";
 export default class CopiBonService {
-  constructor() {
-    this.mainService = MainService.getInstance();
-  }
-  async init() {
-    await this.mainService.init(appTitle,appVersion);
-    this.mf = new MainFrame(this.mainService);
-    //this.mf.initialize(title);
-    ///
-    // const sumModule = await import ("./sum.wasm");
-    // console.log(sumModule.sum(22, 2200));
-  }
-  static main() {
-    const self = new CopiBonService();
-    self.init();
-  }
+	constructor() {
+		this.mainService = MainService.getInstance();
+	}
+	async init() {
+		await this.mainService.init(appTitle, appVersion);
+		this.mf = new MainFrame(this.mainService);
+		//this.mf.initialize(title);
+		///
+		// const sumModule = await import ("./sum.wasm");
+		// console.log(sumModule.sum(22, 2200));
+	}
+	static main() {
+		const self = new CopiBonService();
+		self.init();
+	}
 }
 CopiBonService.main();
