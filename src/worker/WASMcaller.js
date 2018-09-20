7ba8360e99dbea272702e3a9dcbcb82faba54eaf
ryunosinfx <ryunosinfx@livedoor.com>
2018-09-20 09:34:34 +0900

    dekitatokoromade

--- src/worker/WASMcaller.js
+++ src/worker/WASMcaller.js
@@ -1,24 +1,25 @@
-import {BaseWorker} from "./baseWorker";
+import { BaseWorker } from "./baseWorker";
 export class WASMcaller extends BaseWorker {
-  constructor() {
-    super("WASMcaller");
-  }
-  execute() {
-    const msg = "hello WASMcaller! now:" + Date.now();
-    console.log(msg);
-    this.call().catch((e) => {
-      console.log(e)
-    });
-    return msg;
-  }
-  async call() {
-    const response = await fetch('./wasm/sum.wasm');
-    const bytes = await response.arrayBuffer();
-    const module = await WebAssembly.instantiate(bytes, {})
-    const instance = module.instance;
-    console.log(instance.exports.sum(21, 31));
-    const msg = "hello WASMcaller! now:" + Date.now();
-    console.log(msg);
-  };
+	constructor() {
+		super("WASMcaller");
+	}
+	execute() {
+		const msg = "hello WASMcaller! now:" + Date.now();
+		console.log(msg);
+		this.call()
+			.catch((e) => {
+				console.log(e)
+			});
+		return msg;
+	}
+	async call() {
+		const response = await fetch('./wasm/sum.wasm');
+		const bytes = await response.arrayBuffer();
+		const module = await WebAssembly.instantiate(bytes, {})
+		const instance = module.instance;
+		console.log(instance.exports.sum(21, 31));
+		const msg = "hello WASMcaller! now:" + Date.now();
+		console.log(msg);
+	};
 }
 const aWASMcaller = new WASMcaller();
