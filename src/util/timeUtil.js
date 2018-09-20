44fdfb5b569af2fae63b4251d2c0216f4ef40a0c
ryunosinfx <ryunosinfx@livedoor.com>
2018-09-21 00:50:17 +0900

    気持ち修正。

--- src/util/timeUtil.js
+++ src/util/timeUtil.js
@@ -6,15 +6,26 @@ const re_mm = /mm/;
 const re_ss = /ss/;
 
 export const unixTimeToDateFormat = (unixtime, format = "yyyy-MM-dd hh:mm:ss") => {
-  const d = new Date(unixtime);
-  const year = d.getFullYear();
-  const month = d.getMonth() + 1;
-  const day = d.getDate();
-  const hour = ('0' + d.getHours()).slice(-2);
-  const min = ('0' + d.getMinutes()).slice(-2);
-  const sec = ('0' + d.getSeconds()).slice(-2);
-  return format.replace(re_yyyy, year).replace(re_MM,month).replace(re_dd,day).replace(re_hh,hour).replace(re_mm,min).replace(re_ss,sec);
+	const d = new Date(unixtime);
+	const year = d.getFullYear();
+	const month = ('0' + d.getMonth() + 1)
+		.slice(-2);
+	const day = ('0' + d.getDate())
+		.slice(-2);;
+	const hour = ('0' + d.getHours())
+		.slice(-2);
+	const min = ('0' + d.getMinutes())
+		.slice(-2);
+	const sec = ('0' + d.getSeconds())
+		.slice(-2);
+	return format.replace(re_yyyy, year)
+		.replace(re_MM, month)
+		.replace(re_dd, day)
+		.replace(re_hh, hour)
+		.replace(re_mm, min)
+		.replace(re_ss, sec);
 }
 export const getNowUnixtime = () => {
-  new Date().getTime();
+	new Date()
+		.getTime();
 }
