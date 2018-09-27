76d651ce4b4b18dc22474d0f00e507466646189b
ryunosinfx <ryunosinfx@livedoor.com>
2018-09-27 23:50:17 +0900

    dekitatokoromade

--- src/util/pdf/util/binaryUtil.js
+++ src/util/pdf/util/binaryUtil.js
@@ -1,10 +1,59 @@
-
 export class BinaryUtil {
   constructor() {}
-  toArrayBuffer(){
+  toArrayBuffer() {
 
   }
-  convertStr2Ab(str){
+  convertStr2Ab(str) {
+    const string = btoa(unescape(encodeURIComponent(str)));
+    const charList = string.split('');
+    const len = charList.length;
+    const ua = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+      ua[i] = charList[i].charCodeAt(0);
+    }
+    return ua.buffer;
+  }
+  uintToString(ab) {
+  const ua = new Uint8Array(ab);
+  for (var i = 0; i < len; i++) {
+    ua[i] = charList[i].charCodeAt(0);
+  }
 
+    var encodedString = String.fromCharCode.apply(null, uintArray),
+        decodedString = decodeURIComponent(escape(atob(encodedString)));
+    return decodedString;
+}
+  unicodeStringToTypedArray(s) {
+    var escstr = encodeURIComponent(s);
+    var binstr = escstr.replace(/%([0-9A-F]{2})/g, function(match, p1) {
+      return String.fromCharCode('0x' + p1);
+    });
+    var ua = new Uint8Array(binstr.length);
+    Array.prototype.forEach.call(binstr, function(ch, i) {
+      ua[i] = ch.charCodeAt(0);
+    });
+    return ua;
+  }
+  typedArrayToUnicodeString(ua) {
+    var binstr = Array.prototype.map.call(ua, function(ch) {
+      return String.fromCharCode(ch);
+    }).join('');
+    var escstr = binstr.replace(/(.)/g, function(m, p) {
+      var code = p.charCodeAt(p).toString(16).toUpperCase();
+      if (code.length < 2) {
+        code = '0' + code;
+      }
+      return '%' + code;
+    });
+    return decodeURIComponent(escstr);
+  }
+  convertHexToAb(array) {
+    const len = array.length;
+    const retArray = new Uint8Array(len);
+    for (let index = 0; index < len; index++) {
+      const hexStr = array[inde; x];
+      retArray[index] = parseInt(hexStr, 16);
+    }
+    return retArray.buffer;
   }
 }
---
+++ src/util/pdf/util/unicodeEncoder.js
@@ -0,0 +1,125 @@
+export class UnicodeEncoder {
+  static surrogatePairToCodePoint(charCode1, charCode2) {
+    return ((charCode1 & 0x3FF) << 10) + (charCode2 & 0x3FF) + 0x10000;
+  }
+
+  static stringToCodePointArray(str) {
+    const codePoints = [];
+    const len = str.length;
+    for (let i = 0; i < len; i++) {
+      const charCode = str.charCodeAt(i);
+      if ((charCode & 0xF800) === 0xD800) {
+        codePoints.push(UnicodeEncoder.surrogatePairToCodePoint(charCode, str.charCodeAt(++i)));
+      } else {
+        codePoints.push(charCode);
+      }
+    }
+    console.log(codePoints)
+    return codePoints;
+  }
+
+  static codePointArrayToString(codePoints) {
+    let stringParts = "";
+    const len = codePoints.length
+    for (let i = 0; i < len; ++i) {
+      let codePointCharCodes = null;
+      const codePoint = codePoints[i];
+      if (codePoint > 0xFFFF) {
+        const offset = codePoint - 0x10000;
+        codePointCharCodes = [
+          0xD800 + (offset >> 10),
+          0xDC00 + (offset & 0x3FF)
+        ];
+      } else {
+        codePointCharCodes = [codePoint];
+      }
+      stringParts += (String.fromCharCode.apply(String, codePointCharCodes));
+    }
+    return stringParts;
+  }
+  static encodeUTF8(str) {
+    const codes = UnicodeEncoder.stringToCodePointArray(str);
+    const len = codes.length;
+    const array = [];
+    for (let i = 0; i < len; i++) {
+      const c = codes[i];
+      if (c <= 0x7f) { //1byte
+        array.push(c);
+      } else if (c <= 0x7ff) { //2byte
+        array.push(0xc0 | c >>> 6, 0x80 | c & 0xbf);
+      } else if (c <= 0xffff) { //3byte
+        array.push(0xe0 | c >>> 12, 0x80 | c >>> 6 & 0xbf, 0x80 | c & 0xbf);
+      } else if (c <= 0x1ffff) { //4byte
+        array.push(0xf0 | c >>> 18, 0x80 | c >>> 12 & 0xbf, 0x80 | c >>> 6 & 0xbf, 0x80 | c & 0xbf);
+      }
+    }
+    return new Uint8Array(array);
+  }
+  static encodeUTF16(str, endian) {
+    const codes = UnicodeEncoder.stringToCodePointArray(str);
+    const len = codes.length;
+    const array = endian
+      ? endian === 'LE'
+        ? [0xff, 0xfe]
+        : [0xfe, 0xff]
+      : []; //BOM
+    for (let i = 0; i < len; i++) {
+      const c = codes[i];
+      if (endian === 'LE') {
+        array.push(c & 0xff, c >>> 8 & 0xff);
+      } else {
+        array.push(c >>> 8 & 0xff, c & 0xff);
+      }
+    }
+    return new Uint8Array(array);
+  }
+  decodeUTF8(array) {
+    let str = "";
+    const len = array.length;
+    for (let i = 0; i < len; i++) {
+      const c = array[i];
+      if (c <= 0x7f) {
+        str += String.fromCharCode(c);
+      } else if (c <= 0xdf && c >= 0xc2) {
+        str += String.fromCharCode((c & 31) << 6 | array[++i] & 63);
+      } else if (c <= 0xef && c >= 0xe0) {
+        str += String.fromCharCode((c & 15) << 12 | (array[++i] & 63) << 6 | array[++i] & 63);
+      } else if (c <= 0xf7 && c >= 0xf0) {
+        //utf8   11110uuu  10uuxxxx  10xxxxxx  10xxxxxx
+        //utf16  110110wwwwxxxxxx  110111xxxxxxxxxx (wwww = uuuuu-1)
+        str += String.fromCharCode(0xd800 | ((c & 7) << 8 | (array[++i] & 63) << 2 | array[++i] >>> 4 & 3) - 64, 0xdc00 | (array[i++] & 15) << 6 | array[i] & 63);
+      } else {
+        str += String.fromCharCode(0xfffd);
+      }
+    }
+    return str;
+  }
+  decodeUTF16(array) {
+    let endian = 'BE';
+    let i = 0;
+    const len = array.length;
+    let str = "";
+    if (array[0] * array[1] === 0xff * 0xfe) { // remove BOM
+      endian = array[0] === 0xff
+        ? 'LE'
+        : 'BE';
+      i = 2;
+    }
+    for (; i < len; i += 2) {
+      str += String.fromCharCode(
+        endian === 'LE'
+        ? (array[i + 1] << 8 | array[i])
+        : (array[i] << 8 | array[i + 1]));
+    }
+    return str;
+  }
+  static stringToByteArray(str) {
+    const len = str.length;
+    const array = new Uint8Array(len);
+
+    for (let i = 0; i < len; i++) {
+      array[i] = str.charCodeAt(i) & 0xff;
+    }
+    return array;
+  }
+}
