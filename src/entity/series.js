6f6b0593bcb91c6230889aa27ece5b6c9e742d18
ryunosinfx <ryunosinfx@livedoor.com>
2018-12-23 02:08:24 +0900

    seikei

--- src/entity/series.js
+++ src/entity/series.js
@@ -1,15 +1,15 @@
-import {BaseEntity} from "../service/entity/baseEntity";
+import { BaseEntity } from "../service/entity/baseEntity";
 const refcols = ['target']
-export class Series extends BaseEntity{
-  constructor(name) {
-    super("Series");
-    this.name=name;
-    this.target=null;
-    this.no="";
-    this.date="";
-    this.pageNo=0;
-  }
-  getRefCols(){
-    return refcols;
-  }
+export class Series extends BaseEntity {
+	constructor(name) {
+		super("Series");
+		this.name = name;
+		this.target = null;
+		this.no = "";
+		this.date = "";
+		this.pageNo = 0;
+	}
+	getRefCols() {
+		return refcols;
+	}
 }
