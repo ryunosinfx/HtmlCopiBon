8bcd4b563810e4f034da84890a628ee4f521c7de
ryunosinfx <ryunosinfx@livedoor.com>
2023-11-18 18:28:45 +0900

    気持ち省スペース化

--- src/reduxy/action/imageActionCreator.js
+++ src/reduxy/action/imageActionCreator.js
@@ -2,25 +2,17 @@ import { ActionCreator } from '../../util/reactive/actionCreator.js';
 const baseActions = ['addImage', 'removeImage', 'sortImages', 'loadImages'];
 export class ImageActionCreator {
 	constructor() {}
-	static creatAddAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('addImage', targetView, data, storeKey);
-	}
-	static creatRemoveAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('removeImage', targetView, data, storeKey);
-	}
-	static creatLoadImagesAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('loadImages', targetView, data, storeKey);
-	}
-	static creatSortImagesAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('sortImages', targetView, data, storeKey);
-	}
-	static creatChangeTitleImagesAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('changeTitle', targetView, data, storeKey);
-	}
-	static creatDetailAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('detail', targetView, data, storeKey);
-	}
-	static getStoreImagesKey() {
-		return 'imagesData';
-	}
+	static creatAddAction = (targetView, data, storeKey = null) =>
+		ActionCreator.createBaseAction('addImage', targetView, data, storeKey);
+	static creatRemoveAction = (targetView, data, storeKey = null) =>
+		ActionCreator.createBaseAction('removeImage', targetView, data, storeKey);
+	static creatLoadImagesAction = (targetView, data, storeKey = null) =>
+		ActionCreator.createBaseAction('loadImages', targetView, data, storeKey);
+	static creatSortImagesAction = (targetView, data, storeKey = null) =>
+		ActionCreator.createBaseAction('sortImages', targetView, data, storeKey);
+	static creatChangeTitleImagesAction = (targetView, data, storeKey = null) =>
+		ActionCreator.createBaseAction('changeTitle', targetView, data, storeKey);
+	static creatDetailAction = (targetView, data, storeKey = null) =>
+		ActionCreator.createBaseAction('detail', targetView, data, storeKey);
+	static getStoreImagesKey = () => 'imagesData';
 }
