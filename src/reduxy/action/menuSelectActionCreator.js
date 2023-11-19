b2f580e7d150479aa63d4d28467caafdcd0ba759
ryunosinfx <ryunosinfx@livedoor.com>
2023-11-20 01:58:59 +0900

    心持ち省フットプリント化

--- src/reduxy/action/menuSelectActionCreator.js
+++ src/reduxy/action/menuSelectActionCreator.js
@@ -1,10 +1,7 @@
 import { ActionCreator } from '../../util/reactive/actionCreator.js';
 export class MenuSelectActionCreator {
 	constructor() {}
-	static creatSelectAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('selectMenu', targetView, data, storeKey);
-	}
-	static getStoreKey() {
-		return 'menuSelect';
-	}
+	static creatSelectAction = (targetView, data, storeKey = null) =>
+		ActionCreator.createBaseAction('selectMenu', targetView, data, storeKey);
+	static getStoreKey = () => 'menuSelect';
 }
