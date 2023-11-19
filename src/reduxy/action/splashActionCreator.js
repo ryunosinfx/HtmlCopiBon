6c73e75e58eeb0300a0ed7f227c64a2dd43ce6a5
ryunosinfx <ryunosinfx@livedoor.com>
2023-11-20 01:59:15 +0900

    心持ち省フットプリント化

--- src/reduxy/action/splashActionCreator.js
+++ src/reduxy/action/splashActionCreator.js
@@ -1,13 +1,9 @@
 import { ActionCreator } from '../../util/reactive/actionCreator.js';
 export class SplashActionCreator {
 	constructor() {}
-	static creatAddAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('addSplashr', targetView, data, storeKey);
-	}
-	static creatRemoveAction(targetView, data, storeKey = null) {
-		return ActionCreator.createBaseAction('removeSplash', targetView, data, storeKey);
-	}
-	static getStoreKey() {
-		return 'splash';
-	}
+	static getStoreKey = () => 'splash';
+	static creatAddAction = (targetView, data = {}, storeKey = SplashActionCreator.getStoreKey()) =>
+		ActionCreator.createBaseAction('addSplashr', targetView, data, storeKey);
+	static creatRemoveAction = (targetView, data = {}, storeKey = SplashActionCreator.getStoreKey()) =>
+		ActionCreator.createBaseAction('removeSplash', targetView, data, storeKey);
 }
